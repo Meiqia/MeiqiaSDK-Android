bb3687f2d00b79df2021ba5be0579e20f2e9621e
xukq <312965601@qq.com>
2016-03-09 17:43:21 +0800

    add res

---
+++ meiqiasdk/src/main/java/com/meiqia/meiqiasdk/callback/SimpleCallback.java
@@ -0,0 +1,10 @@
+package com.meiqia.meiqiasdk.callback;
+
+/**
+ * 作者:王浩 邮件:bingoogolapple@gmail.com
+ * 创建时间:16/2/29 上午10:32
+ * 描述:
+ */
+public interface SimpleCallback extends OnFailureCallBack {
+    void onSuccess();
+}
